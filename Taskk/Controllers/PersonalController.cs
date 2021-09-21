35d65401261b9a248ee8143084e72204210caaf2
BerkanKmrc1 <70950273+BerkanKmrc1@users.noreply.github.com>
2021-09-21 20:47:14 +0300

    Add project files.

---
+++ Taskk/Controllers/PersonalController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Taskk.Models.Entity;
+namespace Taskk.Controllers
+{
+    public class PersonalController : Controller
+    {
+        TaskDatabaseEntities1 db = new TaskDatabaseEntities1();
+        // GET: Personal
+        public ActionResult PersonalList()
+        {
+            var degerler = db.Personals.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult AddPersonal()
+        {
+        
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddPersonal(Personal p1)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("AddPersonal");
+            }
+            db.Personals.Add(p1);
+            db.SaveChanges();
+            return View("PersonalList");
+        }
+        public ActionResult DeletePersonal(int id)
+        {
+            var personal = db.Personals.Find(id);
+            db.Personals.Remove(personal);
+            db.SaveChanges();
+            return RedirectToAction("PersonalList");
+        }
+        [HttpGet]
+        public ActionResult GetPersonal(int id)
+        {
+            var prsnl = db.Personals.Find(id);
+            return View(prsnl);
+        }
+        [HttpPost]
+        public ActionResult UpdatePersonal(Personal p1)
+        {
+            var personel = db.Personals.Find(p1.ID);
+            personel.Name = p1.Name;
+            personel.Surname = p1.Surname;
+            personel.Email = p1.Email;
+            personel.BirthDay = p1.BirthDay;
+            db.SaveChanges();
+            return RedirectToAction("PersonalList");
+
+        }
+    }
+}
---
+++ Taskk/Controllers/PhoneController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Taskk.Models.Entity;
+namespace Taskk.Controllers
+{
+    public class PhoneController : Controller
+    {
+        TaskDatabaseEntities1 db = new TaskDatabaseEntities1();
+        // GET: Phone
+        public ActionResult PhoneList()
+        {
+            var degerler = db.Phones.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult AddPhone()
+        {
+            List<SelectListItem> degerler = (from i in db.Personals.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.Name,
+                                                 Value = i.ID.ToString()
+
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddPhone(Phone p1)
+        {
+            var prs = db.Personals.Where(m => m.ID == p1.Personal1.ID).FirstOrDefault();
+            p1.Personal1 = prs;
+            db.Phones.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("PhoneList");
+        }
+        public ActionResult DeletePhone(int id)
+        {
+            var phone = db.Phones.Find(id);
+            db.Phones.Remove(phone);
+            db.SaveChanges();
+            return RedirectToAction("PhoneList");
+        }
+    }
+}
